e69f2de9d9062c0e860f4c899c428baf75f3bd29
ignasvilkas <ignas.pilybas@icloud.com>
2024-11-04 09:12:06 +0000

    Added database models for Book and Review

---
+++ models.py
@@ -0,0 +1,19 @@
+# models.py
+from flask_sqlalchemy import SQLAlchemy
+from app import app
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/book_reviews_db'
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    cover_image = db.Column(db.String(200))
+    amazon_link = db.Column(db.String(200))
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
+    comment = db.Column(db.Text, nullable=False)
+    upvotes = db.Column(db.Integer, default=0)
